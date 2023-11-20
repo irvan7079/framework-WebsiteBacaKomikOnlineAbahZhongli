68d484e80e1325134d533d67b704514379ea5666
muhammadirvanhakim70@gmail.com <muhammadirvanhakim70@gmail.com>
2023-11-20 23:19:25 +0800

    .

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,11 +1,4 @@
 {{-- Hero --}}
 <section class="h-[500px] width-full relative">
-    <img src="{{ asset('assets/images/jujutsu kaisen.jpg') }}" alt="jujutsu kaisen"
-        class="absolute w-full h-full ">
-    <div  class="info">
-        <h2>Jujutsu Kaisen</h2>
-        <p><a href="dataKomik/Jujutsu Kaisen Vol.01.pdf" target="_blank" class="me">Baca Sekarang!</a>
-        </p>
-        <br>
-    </div>
+    <img src="{{ asset('assets/images/jujutsu kaisen.jpg') }}" alt="jujutsu kaisen" class="absolute w-full h-full ">
 </section>
